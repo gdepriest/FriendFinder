574e9fdb9f7c57b5cfe0ad22972e52562487e232
Genevieve DePriest <gdpparton@gmail.com>
2019-07-17 18:52:00 -0600

    committing fixes to routes

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsArray = [
+    {
+        name: "Genevieve",
+        photo: "link",
+        answers: [5, 5, 5, 1, 1, 1, 5, 1, 4, 5, 2]
+    }
+]
+
+module.exports = friendsArray
--- server.js
+++ server.js
@@ -8,13 +8,17 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/home.html"));
-});
+// app.get("/survey", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+// });
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
-});
+// app.use(function(req, res) {
+//     res.sendFile(path.join(__dirname, "./app/public/home.html"));
+// });
+
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, () => {
     console.log(`API Server now listening on PORT ${PORT}!`);
